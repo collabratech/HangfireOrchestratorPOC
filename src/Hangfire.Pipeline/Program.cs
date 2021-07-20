fa463948ebb76657458537c60febf30509e80dea
Muller <marcos.muller@collabratechnology.com>
2021-07-20 10:41:44 -0300

    add Hangfire Console with hangfire server and API projects to the solution

---
+++ src/Hangfire.Pipeline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hangfire.Pipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
