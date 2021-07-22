60321c3a6e5c53757286f8b881b822adbc9ad0d3
Muller <marcos.muller@collabratechnology.com>
2021-07-22 11:16:45 -0300

    pipeline worked, ready to tests

--- GenericPipeline/Program.cs
+++ GenericPipeline/Program.cs
@@ -4,20 +4,21 @@ using System.Threading;
 using System.Threading.Tasks;
 using Hangfire;
 using Hangfire.SqlServer;
-using Microsoft.Owin.Hosting;
 
 namespace GenericPipeline
 {
-    public static class Program
-    {
-        public static void Main()
-        {
-            GlobalConfiguration.Configuration
-                .UseColouredConsoleLogProvider()
-                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-                .UseSimpleAssemblyNameTypeSerializer()
-                .UseRecommendedSerializerSettings()
-                .UseResultsInContinuations()
+	public static class Program
+	{
+		[Obsolete]
+		public static void Main()
+		{
+			GlobalConfiguration.Configuration
+				.UseColouredConsoleLogProvider()
+				.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+				.UseSimpleAssemblyNameTypeSerializer()
+				.UseRecommendedSerializerSettings()
+				.UseResultsInContinuations()
+
 				.UseSqlServerStorage(@"Data Source = DESKTOP-P0P0RVI\SQLEXPRESS; Initial Catalog = Hangfire; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False", new SqlServerStorageOptions
 				{
 					CommandBatchMaxTimeout = TimeSpan.FromMinutes(1),
@@ -29,218 +30,74 @@ namespace GenericPipeline
 					EnableHeavyMigrations = true
 				});
 
-            var backgroundJobs = new BackgroundJobClient
-            {
-                RetryAttempts = 5
-            };
+			var backgroundJobs = new BackgroundJobClient
+			{
+				RetryAttempts = 5
+			};
 
-            var job1 = BackgroundJob.Enqueue<Services>(x => x.WriteIndex(0));
-            var job2 = BackgroundJob.ContinueJobWith<Services>(job1, x => x.WriteIndex(null));
-            var job3 = BackgroundJob.ContinueJobWith<Services>(job2, x => x.WriteIndex(null));
-            var job4 = BackgroundJob.ContinueJobWith<Services>(job3, x => x.WriteIndex(null));
-            var job5 = BackgroundJob.ContinueJobWith<Services>(job4, x => x.WriteIndex(null));
+			//for (int i = 1; i <= 5; i++)
+			//{
+			var parentJobId = BackgroundJob.Enqueue(() => FireAndForget());
+			var jobId = BackgroundJob.Schedule(() => Delayed(), TimeSpan.FromSeconds(30));
+			BackgroundJob.ContinueWith(jobId, () => ContinueWith());
 
-            RecurringJob.AddOrUpdate("seconds", () => Console.WriteLine("Collabra, seconds!"), "*/15 * * * * *");
-            RecurringJob.AddOrUpdate(() => Console.WriteLine("Collabra, pipeline!"), Cron.Minutely);
-            RecurringJob.AddOrUpdate("hourly", () => Console.WriteLine("Collabra"), "25 15 * * *");
-            RecurringJob.AddOrUpdate("neverfires", () => Console.WriteLine("Can only be triggered"), "0 0 31 2 *");
+			var id1 = BackgroundJob.Enqueue(() => ExecuteThis(1));
 
-            RecurringJob.AddOrUpdate("Brazilian", () => Console.WriteLine("Brazilian"), "15 08 * * *", TimeZoneInfo.FindSystemTimeZoneById("Central Brazilian Standard Time"));
-            RecurringJob.AddOrUpdate("UTC", () => Console.WriteLine("UTC"), "15 10 * * *");
-            RecurringJob.AddOrUpdate("Brazil", () => Console.WriteLine("Brazil"), "15 11 * * *", TimeZoneInfo.Local);
+			var id2 = BackgroundJob.ContinueWith(id1, () => ExecuteThis(2));
 
-            using (WebApp.Start<Startup>("http://localhost:5010"))
-            {
-                var count = 1;
+			var id3 = BackgroundJob.ContinueWith(id2, () => ExecuteThis(3));
 
-                while (true)
-                {
-                    var command = Console.ReadLine();
+			BackgroundJob.ContinueWith(id3, () => ExecuteThis(4));
 
-                    if (command == null || command.Equals("stop", StringComparison.OrdinalIgnoreCase))
-                    {
-                        break;
-                    }
+			RecurringJob.AddOrUpdate(() => Recurring(), Cron.MinuteInterval(1));
+			//}
 
-                    if (command.StartsWith("add", StringComparison.OrdinalIgnoreCase))
-                    {
-                        try
-                        {
-                            var workCount = int.Parse(command.Substring(4));
-                            for (var i = 0; i < workCount; i++)
-                            {
-                                var number = i;
-                                BackgroundJob.Enqueue<Services>(x => x.Random(number));
-                            }
-                            Console.WriteLine("Jobs enqueued.");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                        }
-                    }
+			using (var server = new BackgroundJobServer())
+			{
+				Console.ReadLine();
+			}
 
-                    if (command.StartsWith("async", StringComparison.OrdinalIgnoreCase))
-                    {
-                        try
-                        {
-                            var workCount = int.Parse(command.Substring(6));
-                            for (var i = 0; i < workCount; i++)
-                            {
-                                BackgroundJob.Enqueue<Services>(x => x.Async(CancellationToken.None));
-                            }
-                            Console.WriteLine("Jobs enqueued.");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                        }
-                    }
+			Console.WriteLine("Press to exit...");
+			Console.ReadKey();
+			CloseServer();
+		}
 
-                    if (command.StartsWith("static", StringComparison.OrdinalIgnoreCase))
-                    {
-                        try
-                        {
-                            var workCount = int.Parse(command.Substring(7));
-                            for (var i = 0; i < workCount; i++)
-                            {
-                                BackgroundJob.Enqueue(() => Console.WriteLine("Collabra, {0}!", "pipeline"));
-                            }
-                            Console.WriteLine("Jobs enqueued.");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                        }
-                    }
+		[DisableConcurrentExecution(15)]
+		public static void ExecuteThis(int number)
+		{
+			Console.WriteLine("Start " + number);
+			Console.WriteLine("End " + number);
+		}
 
-                    if (command.StartsWith("error", StringComparison.OrdinalIgnoreCase))
-                    {
-                        var workCount = int.Parse(command.Substring(6));
-                        for (var i = 0; i < workCount; i++)
-                        {
-                            BackgroundJob.Enqueue<Services>(x => x.Error());
-                        }
-                    }
+		public static void FireAndForget()
+		{
+			Console.WriteLine("Executing some Task -> FireForget!");
+		}
 
-                    if (command.StartsWith("args", StringComparison.OrdinalIgnoreCase))
-                    {
-                        var workCount = int.Parse(command.Substring(5));
-                        for (var i = 0; i < workCount; i++)
-                        {
-                            BackgroundJob.Enqueue<Services>(x => x.Args(Guid.NewGuid().ToString(), 14442, DateTime.UtcNow));
-                        }
-                    }
+		//Delayed
+		//This type of Job is executed only once and is triggered after a TimeSpan (specified interval of time).
+		public static void Delayed()
+		{
+			Console.WriteLine("Executing some Task -> Delayed !");
+		}
 
-                    if (command.StartsWith("custom", StringComparison.OrdinalIgnoreCase))
-                    {
-                        var workCount = int.Parse(command.Substring(7));
-                        for (var i = 0; i < workCount; i++)
-                        {
-                            BackgroundJob.Enqueue<Services>(x => x.Custom(
-                                new Random().Next(),
-                                new[] { "Collabra", "pipeline!" },
-                                new Services.CustomObject { Id = 123 },
-                                DayOfWeek.Friday
-                                ));
-                        }
-                    }
+		//Continuations
+		//This type of Job is executed when its specified parent job has finished its execution.
+		public static void ContinueWith()
+		{
+			Console.WriteLine("Executing some Task -> Continue/Child!");
+		}
 
-                    if (command.StartsWith("fullargs", StringComparison.OrdinalIgnoreCase))
-                    {
-                        var workCount = int.Parse(command.Substring(9));
-                        for (var i = 0; i < workCount; i++)
-                        {
-                            BackgroundJob.Enqueue<Services>(x => x.FullArgs(
-                                false,
-                                123,
-                                'c',
-                                DayOfWeek.Monday,
-                                "Collabra",
-                                new TimeSpan(0, 1, 10),
-                                new DateTime(2021, 07, 21),
-                                new Services.CustomObject { Id = 123 },
-                                new[] { "1", "2", "3" },
-                                new[] { 4, 5, 6 },
-                                new long[0],
-                                null,
-                                new List<string> { "7", "8", "9" }));
-                        }
-                    }
 
-                    if (command.StartsWith("in", StringComparison.OrdinalIgnoreCase))
-                    {
-                        var seconds = int.Parse(command.Substring(2));
-                        var number = count++;
-                        BackgroundJob.Schedule<Services>(x => x.Random(number), TimeSpan.FromSeconds(seconds));
-                    }
-
-                    if (command.StartsWith("cancelable", StringComparison.OrdinalIgnoreCase))
-                    {
-                        var iterations = int.Parse(command.Substring(11));
-                        BackgroundJob.Enqueue<Services>(x => x.Cancelable(iterations, JobCancellationToken.Null));
-                    }
-
-                    if (command.StartsWith("delete", StringComparison.OrdinalIgnoreCase))
-                    {
-                        var workCount = int.Parse(command.Substring(7));
-                        for (var i = 0; i < workCount; i++)
-                        {
-                            var jobId = BackgroundJob.Enqueue<Services>(x => x.EmptyDefault());
-                            BackgroundJob.Delete(jobId);
-                        }
-                    }
-
-                    if (command.StartsWith("fast", StringComparison.OrdinalIgnoreCase))
-                    {
-                        try
-                        {
-                            var workCount = int.Parse(command.Substring(5));
-                            Parallel.For(0, workCount, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount }, i =>
-                            {
-                                if (i % 2 == 0)
-                                {
-                                    BackgroundJob.Enqueue<Services>(x => x.EmptyCritical());
-                                }
-                                else
-                                {
-                                    BackgroundJob.Enqueue<Services>(x => x.EmptyDefault());
-                                }
-                            });
-                            Console.WriteLine("Jobs enqueued.");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                        }
-                    }
-
-                    if (command.StartsWith("generic", StringComparison.OrdinalIgnoreCase))
-                    {
-                        BackgroundJob.Enqueue<GenericServices<string>>(x => x.Method("Collabra", 1));
-                    }
-
-                    if (command.StartsWith("continuations", StringComparison.OrdinalIgnoreCase))
-                    {
-                        WriteString("Collabra, Hangfire continuations!");
-                    }
-                }
-            }
-
-            Console.WriteLine("Press Enter to exit...");
-            Console.ReadLine();
-        }
-
-        public static void WriteString(string value)
-        {
-            var lastId = BackgroundJob.Enqueue<Services>(x => x.Write(value[0]));
-
-            for (var i = 1; i < value.Length; i++)
-            {
-                lastId = BackgroundJob.ContinueJobWith<Services>(lastId, x => x.Write(value[i]));
-            }
-
-            BackgroundJob.ContinueJobWith<Services>(lastId, x => x.WriteBlankLine());
-        }
-    }
+		//Recurring
+		//This type of Job is executed multiple times and is triggered based upon the recurrence pattern (Hangfire.Cron type).
+		public static void Recurring()
+		{
+			Console.WriteLine("Executing some Task -> Recurring!");
+		}
+		public static void CloseServer()
+		{
+			Console.WriteLine("Closing server");
+		}
+	}
 }
